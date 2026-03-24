690edd93f2be8973b32a6f0a2f5c7953869c56db
Sabaroche <S567597@nwmissouri.edu>
2026-03-23 22:26:35 -0500

    updated users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var vOne = 10
+var vTwo = 15
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  vTwo -= vOne
+  res.send('Values are: ' + vOne + ' and ' + vTwo);
 });
 
 module.exports = router;
